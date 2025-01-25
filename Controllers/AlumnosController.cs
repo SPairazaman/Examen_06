8e7fb934f95809e612449895346351956e19ef26
Santos Pairazaman <spairazaman@hotmail.com>
2025-01-24 21:16:46 -0500

    Creación de Controller Alumnos

---
+++ Controllers/AlumnosController.cs
@@ -0,0 +1,84 @@
+﻿using Domain.Models;
+using Examen_06.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Examen_06.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        public IActionResult Index()
+        {
+            AlumnoService alumnoService = new AlumnoService();
+            var alumnos = alumnoService.Listar();
+
+            var model = alumnos.Select(x => new AlumnoModel
+            {
+                AlumnoID=x.AlumnoID,
+                Nombre=x.Nombre,
+                Apellido=x.Apellido,
+                FechaNacimiento=x.FechaNacimiento,
+                CorreoElectronico=x.CorreoElectronico
+            
+            }).ToList();
+
+           return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AlumnoModel model )
+        {
+            if (ModelState.IsValid) { 
+                AlumnoService alumnoService=new AlumnoService();
+
+                var dominio = new Alumno
+                {
+                    AlumnoID = model.AlumnoID,
+                    Nombre = model.Nombre,
+                    Apellido = model.Apellido,
+                    FechaNacimiento = model.FechaNacimiento,
+                    CorreoElectronico = model.CorreoElectronico
+                };
+
+
+                alumnoService.Insertar(dominio);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id) {
+            AlumnoService alumnoService = new AlumnoService();
+            Alumno alumno =alumnoService.BuscarPorId(id);
+
+            AlumnoModel alumnoModel = new AlumnoModel();
+
+            alumnoModel.AlumnoID= alumno.AlumnoID;
+            alumnoModel.Nombre= alumno.Nombre;
+            alumnoModel.Apellido= alumno.Apellido;
+            alumnoModel.FechaNacimiento=alumno.FechaNacimiento;
+            alumnoModel.CorreoElectronico= alumno.CorreoElectronico;
+
+            return View(alumnoModel);
+
+        }
+
+        [HttpPost]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(int id)
+        {
+            AlumnoService alumnoService = new AlumnoService();
+            alumnoService.Eliminar(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ Models/ProfesorModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Examen_06.Models
+{
+    public class ProfesorModel
+    {
+        public int ProfesorID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Especialidad { get; set; }
+        public string CorreoElectronico { get; set; }
+    }
+}
